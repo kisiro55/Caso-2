98fafdcd24a07731966f70954bb2993db56df58f
Eze_Lanza <ezequiel.lanza@gmail.com>
2020-08-22 17:57:59 -0300

    Arboles

--- 1-Aportes individuales/Decision Trees _Eze.R
+++ 1-Aportes individuales/Decision Trees _Eze.R
@@ -2,7 +2,9 @@ library (C50)
 library(foreign)
 library(caret)
 library(dplyr)
-install.packages("dplyr")
+library(tidyverse)
+
+
 
 ## FUNCION PARA ENTRENAMIENTO
 
@@ -20,24 +22,25 @@ Entrenamiento <- function(DFTrain, DFTags, ConfLev, MinNum){
 # OPCION 2 : CATEGORIZAR ALGUNAS VARIABLES
 
 Subs.Train.V2 <- subscriptores.training
+Subs.Test.V2 <- subscriptores.test
 
 ## MODIFICACION DE LA VARIABLLE DE LLAMADAS AL CENTRO DE CONTACTO
 Subs.Train.V2$CustServ_Calls <- as.factor(Subs.Train.V2$CustServ_Calls)
+Subs.Test.V2$CustServ_Calls <- as.factor(Subs.Test.V2$CustServ_Calls)
 
 levels(Subs.Train.V2$CustServ_Calls)
 normales <- c("0", "1", "2", "3")                    
 quejosos <- c( "4", "5", "6","7", "8", "9")
 
 Subs.Train.V2$CustServ_Calls <- ifelse(Subs.Train.V2$CustServ_Calls %in% normales, "Normales", "Quejosos")
-table(Subs.Train.V2$CustServ_Calls)
+Subs.Test.V2$CustServ_Calls <- ifelse(Subs.Test.V2$CustServ_Calls %in% normales, "Normales", "Quejosos")
+
 
 # QUITAR VARIABLES QUE NO SUMAN 
 
-Subs.Train.V2 <- select(Subs.Train.V2, -Phone, -Mahalanobis)
+Subs.Train.V2 <-  select( Subs.Train.V2,-Phone, -Mahalanobis,- Outlier, -Test_Split)
+Subs.Test.V2 <-  select( Subs.Test.V2,-Phone, -Mahalanobis,- Outlier, -Test_Split)
 
-colnames(Subs.Train.V2)
-
-?filter
 # OPCION C : AGRUPAR VARIABLES CUALI EN CUANTI CADA UNA POR SEPARADO.
 
 ################################################
@@ -46,6 +49,7 @@ colnames(Subs.Train.V2)
 #                                              #
 ################################################
 
+#####################   OPCION 1    ################################
 # Estandarizo el training
 idx <- which(sapply(subscriptores.training[,1:17], class) %in% c("numeric","integer"))
 Sub_Train_EST1 <- scale(subscriptores.training[,idx])
@@ -59,6 +63,18 @@ Sub_Test_EST1 <- scale(subscriptores.test[,idx])
 Sub_Test_EST1 <- cbind(Sub_Test_EST1,subscriptores.test[,-idx] ) 
 #Elimino varibale Phone
 Sub_Test_EST1 <- Sub_Test_EST1[,-14]
+
+#####################   OPCION 2    ################################
+# Estandarizo el training
+idx <- which(sapply(Subs.Train.V2[,1:ncol(Subs.Train.V2)], class) %in% c("numeric","integer"))
+Sub_Train_EST2 <- scale(Subs.Train.V2[,idx])
+Sub_Train_EST2 <- cbind(Sub_Train_EST2,Subs.Train.V2[,-idx] ) 
+
+#Estandarizar Test
+idx <- which(sapply(Subs.Test.V2[,1:ncol(Subs.Test.V2)], class) %in% c("numeric","integer"))
+Sub_Test_EST2 <- scale(Subs.Test.V2[,idx])
+Sub_Test_EST2 <- cbind(Sub_Test_EST2,Subs.Test.V2[,-idx] ) 
+
 
 ################################################
 #
@@ -98,15 +114,42 @@ for (i in 1:10){
                 matrizConf <- confusionMatrix(data = PredITE, reference = Sub_Test_EST1$Churn, positive = "True.")
           
                 Datos <- c(ModeloITE$control$CF,ModeloITE$control$minCases,
-                           ModeloITE$size,matrizConf$byClass)                                                      
+                           ModeloITE$size,matrizConf$byClass,matrizConf$overall )                                                      
                 Resultados <- rbind(Resultados,Datos)   
                 pos <- pos + 1
                 }
 }
 Resultados <- Resultados[-c(1),] #ELIMINO PRIMERA FILA QUE POR EL RBIND SE COLOCO
 
+
 head(Resultados)
 
+## Grafico, deteccion de churners vs Accuracy 
+
+######
+# ITERACION CON DATA SET 2
+#####
+Resultadosv2 <- 0
+pos <- 0
+
+for (i in 1:10){
+        for (j in 1:length(ConfBase)) {
+                Conf  <- ConfBase[j]
+                ModeloITE <-  Entrenamiento(Sub_Train_EST2[1:15],Sub_Train_EST2$Churn, Conf,i )            #GENERAR MODELO
+                PredITE <- predict(ModeloITE, Sub_Test_EST2[1:15])
+                
+                matrizConf <- confusionMatrix(data = PredITE, reference = Sub_Train_EST2$Churn, positive = "True.")
+                
+                Datos <- c(ModeloITE$control$CF,ModeloITE$control$minCases,
+                           ModeloITE$size,matrizConf$byClass,matrizConf$overall )                                                      
+                Resultadosv2 <- rbind(Resultadosv2,Datos)   
+                pos <- pos + 1
+        }
+}
+Resultadosv2 <- Resultadosv2[-c(1),] #ELIMINO PRIMERA FILA QUE POR EL RBIND SE COLOCO
+
+
+head(Resultadosv2)
 
 ## SE DEBERIA ELEGIR EL QUE MEJOR ELIJA A LOS POSIBLES CHURN
 
